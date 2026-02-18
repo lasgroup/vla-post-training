69038f88f4633c29cf1623a84908fab0770c0c9a
Lenart Treven <lenart.treven44@gmail.com>
2026-02-18 19:10:44 +0100

    Fix init

--- src/rl/networks/encoders/__init__.py
+++ src/rl/networks/encoders/__init__.py
@@ -1,3 +1,5 @@
 from src.rl.networks.mlp import MLP
-from src.rl.networks.normal_policy import NormalPolicy
+from src.rl.networks.decoders.policies.normal_policy import (
+    NormalPolicyDecoder as NormalPolicy,
+)
 from src.rl.networks.normal_tanh_policy import NormalTanhPolicy
