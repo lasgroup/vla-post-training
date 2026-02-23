cddaec756e8ba35565e70337e5988351969c157c
lenarttreven <lenart.treven44@gmail.com>
2026-02-23 10:38:45 +0100

    Log compilations

--- scripts/train_online_with_awr_agent.py
+++ scripts/train_online_with_awr_agent.py
@@ -36,8 +36,8 @@ if mp.current_process().name != "MainProcess":
 
 # Avoid aggressive JAX GPU preallocation in the trainer process.
 # os.environ.setdefault("XLA_PYTHON_CLIENT_PREALLOCATE", "false")
+os.environ["JAX_LOG_COMPILES"] = "1"
 os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = "0.9"
-
 
 import gc
 import platform
