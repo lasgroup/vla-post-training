823a75004828f436937b0aa97c348bf01b79ba38
sukhijab <sukhijab@student.ethz.ch>
2026-02-24 19:20:29 +0100

    Reduced log interval in the bash script

--- submit_train.sh
+++ submit_train.sh
@@ -10,7 +10,7 @@ WANDB_PROJECT="${WANDB_PROJECT:-openpi}"
 TRAIN_SEED="${TRAIN_SEED:-0}"
 TRAIN_CONFIG="${TRAIN_CONFIG:-pi05_libero_online_aw_sft}"
 CHECKPOINT_BASE_DIR="${CHECKPOINT_BASE_DIR:-/capstor/scratch/cscs/${USER}/checkpoints}"
-
+LOG_INTERVAL="${LOG_INTERVAL:-50}"
 # Added --account=a143 to srun as well to ensure the container inherits it.
 srun --account=a143 --environment=vla-post-training \
 uv run scripts/train_online_with_awr_agent.py "${TRAIN_CONFIG}" \
@@ -19,4 +19,5 @@ uv run scripts/train_online_with_awr_agent.py "${TRAIN_CONFIG}" \
 --exp_name "${EXP_NAME}" \
 --project_name "${WANDB_PROJECT}" \
 --seed "${TRAIN_SEED}" \
---collect.seed "${TRAIN_SEED}"
+--collect.seed "${TRAIN_SEED}" \
+--log_interval "${LOG_INTERVAL}"
