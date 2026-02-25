154f85a70e1e3ca36f08d5cc5fa3c84991f42226
sukhijab <sukhijab@student.ethz.ch>
2026-02-25 07:15:42 +0100

    changed submission file to collect much more episodes for the critic training and update critic for longer before data collection

--- submit_train.sh
+++ submit_train.sh
@@ -11,6 +11,9 @@ TRAIN_SEED="${TRAIN_SEED:-0}"
 TRAIN_CONFIG="${TRAIN_CONFIG:-pi05_libero_online_aw_sft}"
 CHECKPOINT_BASE_DIR="${CHECKPOINT_BASE_DIR:-/capstor/scratch/cscs/${USER}/checkpoints}"
 BUFFER_CAPACITY="${BUFFER_CAPACITY:-250000}"
+POLICY_START_TRAINING="${POLICY_START_TRAINING:-1000}"
+NUM_ROLLOUTS = "${NUM_ROLLOUTS: 1000}"
+COLLECT_INTERVAL = "${COLLECT_INTERVAL: 1000}"
 LOG_INTERVAL="${LOG_INTERVAL:-50}"
 # Added --account=a143 to srun as well to ensure the container inherits it.
 srun --account=a143 --environment=vla-post-training \
@@ -22,4 +25,7 @@ uv run scripts/train_online_with_awr_agent.py "${TRAIN_CONFIG}" \
 --seed "${TRAIN_SEED}" \
 --collect.seed "${TRAIN_SEED}" \
 --log_interval "${LOG_INTERVAL}" \
+--collect.num_rollouts "${NUM_ROLLOUTS}" \
+--collect.collect_interval "${COLLECT_INTERVAL}" \
+--rl.policy_training_start_step "${POLICY_START_TRAINING}" \
 --rl.buffer_capacity "${BUFFER_CAPACITY}"
