3aa2834489276eb9404d34771a586a5cfeacd42b
sukhijab <sukhijab@student.ethz.ch>
2026-03-18 10:49:34 +0100

    changed launcher to only use MC for training

--- scripts/awr_agent/launcher.py
+++ scripts/awr_agent/launcher.py
@@ -45,17 +45,17 @@ NUM_TRAIN_STEPS = 5_000
 # Keys can be any `_config.cli()` override.
 # If this dict is empty, one run is launched with config defaults.
 applicable_configs: Dict[str, List[Any]] = {
-    "seed": [0, 1, 2],
+    "seed": [0, 2],
     "log_interval": [25],
     "rl.num_critic_updates_per_batch": [
-        10,
+        10, 100, 1000
     ],
     "collect.use_time_to_success_as_reward": [True],
     "batch_size": [256],
     "rl.policy_training_start_step": [900],
     "rl.online_ratio": [1.0],
     "rl.reset_policy_params_to_ema_period": [500],
-    "rl.use_mc_returns": [True, False],
+    "rl.use_mc_returns": [True],
     "collect.num_initial_rollouts": [5],
     "lr_schedule.value": [2.5e-5],
     "rl.td_weight_schedule.switch_step": [1_000_000],
