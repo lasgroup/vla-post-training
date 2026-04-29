07897cd0f1631d3a832795147189bbf8cdea45eb
ralfroemer99 <ralf-roemer@gmx.de>
2026-04-29 10:51:09 +0200

    Add sweep tag to launcher 2

--- scripts/best_of_n_agent/launcher.py
+++ scripts/best_of_n_agent/launcher.py
@@ -26,11 +26,11 @@ SCRIPT = "scripts/best_of_n_agent/exp.py"
 CONFIG_NAME = "pi05_libero_online_best_of_n"
 PROJECT_NAME = "value_learning"
 DEFAULT_LOG_INTERVAL = 25
-DEFAULT_SAVE_INTERVAL = 20_000      # 5_000
+DEFAULT_SAVE_INTERVAL = 5_000      # 20_000
 DEFAULT_SEED = 0
 DEFAULT_BUFFER_CAPACITY = 250000
 DEFAULT_NUM_ROLLOUTS = 1
-DEFAULT_COLLECT_INTERVAL = 100_000     # 300
+DEFAULT_COLLECT_INTERVAL = 300     # 100_000
 DEFAULT_NUM_CRITIC_UPDATES_PER_BATCH = 10
 DEFAULT_USE_TIME_TO_SUCCESS_AS_REWARD = True
 DEFAULT_BATCH_SIZE = 256
@@ -40,9 +40,9 @@ DEFAULT_TRAIN_ENV_NUM = 4
 DEFAULT_TASKS = ["libero_90_59-62"]
 DEFAULT_EVAL_TASKS = ["libero_90_59-62"]
 DEFAULT_EVAL_ENV_NUM = 4
-DEFAULT_EVAL_INTERVAL = 2000         # 300
+DEFAULT_EVAL_INTERVAL = 300         # 2000
 DEFAULT_NUM_EVAL_ROLLOUTS = 32
-NUM_TRAIN_STEPS = 20_000              # 5_000
+NUM_TRAIN_STEPS = 5_000              # 20_000
 DEFAULT_CRITIC_TRAINING_START_STEP = 0
 DEFAULT_CRITIC_INFERENCE_START_STEP = 900
 DEFAULT_NUM_CPUS = 16
@@ -52,26 +52,27 @@ DEFAULT_NUM_CPUS = 16
 # If this dict is empty, one run is launched with config defaults.
 applicable_configs: Dict[Union[str, tuple], List[Any]] = {
     # General
+    "tags": [["utd_sweep_04_29"]],
     "seed": [0, 1, 2, 3, 4],  # [0, 1, 2, 3, 4]
     # Data collection/eval
-    "collect.num_rollouts": [256],        # [16]
+    "collect.num_rollouts": [16],        # [256]
     # Critic training
     "collect.use_time_to_success_as_reward": [True],
     "lr_schedule.value": [5e-5],
-    "rl.num_critic_updates_per_batch": [10],
-    "rl.num_offline_pretraining_steps": [0, 10_000],
+    "rl.num_critic_updates_per_batch": [1, 5, 10, 20],
+    "rl.num_offline_pretraining_steps": [0],
     "rl.td_weight_schedule.init_value": [0.5],    # Constant 0.5 * td + 0.5 * mc
     "rl.td_weight_schedule.end_value": [0.5],
     "rl.td_weight_schedule.switch_step": [500_000],      # If -1, will be set to match critic_inference_start_step
     "rl.num_value_bins": [1],                       # Loss-specific
     # "rl.value_target_type": ["two_hot"],
     # Ablation: frozen pi0 prefix embeddings vs. trainable ResNet encoder
-    "rl.critic_encoder_type": ["resnet", "pi0_prefix_resnet"],       # ["pi0_prefix", "resnet", "pi0_prefix_resnet"],
+    "rl.critic_encoder_type": ["pi0_prefix"],       # ["pi0_prefix", "resnet", "pi0_prefix_resnet"],
     # "rl.train_pi0_prefix_encoder": [True],
     # "rl.pi0_encoder_lr_schedule.value": [1e-5],
     # "rl.critic_action_compress_dim": [32],          # Compresses 320-dim flat action chunk to 32 before concatenation with obs embedding
     # Best-of-N specific
-    "rl.train_on_policy_value_function": [True],
+    "rl.train_on_policy_value_function": [False],
     "rl.critic_inference_start_step": [0],
     "rl.n_samples": [32],
     # Single-task
