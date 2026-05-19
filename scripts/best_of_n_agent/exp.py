2ab6c1483b10857a2509687a4bcf8ea6a60eeea9
Marco Bagatella <mbagatella@ethz.ch>
2026-05-19 08:44:06 +0200

    remove determinism flag

--- scripts/best_of_n_agent/exp.py
+++ scripts/best_of_n_agent/exp.py
@@ -1,8 +1,8 @@
 # ruff: noqa: E402
 
 # uncomment to force determinism
-import os
-os.environ["XLA_FLAGS"] = os.environ.get("XLA_FLAGS", "") + " --xla_gpu_deterministic_ops=true"
+# import os
+# os.environ["XLA_FLAGS"] = os.environ.get("XLA_FLAGS", "") + " --xla_gpu_deterministic_ops=true"
 
 # suppress Numba FNV hashing warnings
 import warnings
